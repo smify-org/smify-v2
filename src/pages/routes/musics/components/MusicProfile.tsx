58c067ed7f432aba3c7445bd772a7a47d05a25d0
Hope <pvpswapedro123@gmail.com>
2023-09-04 09:34:06 -0300

    styles: ajustes de sombras

--- src/pages/routes/musics/components/MusicProfile.tsx
+++ src/pages/routes/musics/components/MusicProfile.tsx
@@ -5,6 +5,7 @@ import { useEffect } from 'react'
 import IMusics from '#/interfaces/IMusics'
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import TuneIcon from '@mui/icons-material/Tune';
 
 
 export default function MusicProfile({
@@ -27,14 +28,15 @@ export default function MusicProfile({
         getMusics()
     }, [musicId])
 
+
     useEffect(() => {
         console.log('Buscando favoritos')
         const getFavoriteMusics = async () => {
             const data = await EndPoints.getFavoriteMusics()
             console.log('Musicas favoritas', data)
-            
-            const isFavorite = data.some((item: {favorite_music_id: number}) => item.favorite_music_id === parseInt(musicId))
-            
+
+            const isFavorite = data.some((item: { favorite_music_id: number }) => item.favorite_music_id === parseInt(musicId))
+
             console.log('isFavorite', isFavorite)
 
             setIsFavorite(isFavorite)
@@ -48,41 +50,54 @@ export default function MusicProfile({
 
     return (
         <div className='music_container p-3' style={{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            backgroundColor: colors[3],
+            backgroundColor: colors[0],
+            boxShadow: 'inset 0px -181px 147px -67px rgba(52, 152, 219, 0.5)'
         }}>
-            <div>
-                {
-                    music.map(item => (
-                        <div key={item.id} className="music_profile-container">
-                            <div>
-                                <ColorExtractor getColors={(colors: string[]) => setColors(colors)}>
-                                    <img src={item.logo} alt="" className="rounded img-fluid" />
-                                </ColorExtractor>
+            <div style={{
+                display: 'flex',
+                alignItems: 'flex-end'
+            }}>
+                <div>
+                    {
+                        music.map(item => (
+                            <div key={item.id} className="music_profile-container">
+                                <div className='music_profile-image'>
+                                    <ColorExtractor getColors={(colors: string[]) => setColors(colors)}>
+                                        <img src={item.logo} alt="" />
+                                    </ColorExtractor>
+                                </div>
+                                <div className='music_profile-info'>
+                                    <h1>{item.name}</h1>
+                                    <p>{item.singer}</p>
+                                </div>
                             </div>
-                            <div>
-                                <h1>{item.name}</h1>
-                                <p>{item.singer}</p>
-                            </div>
-                        </div>
-                    ))
-                }
-            </div>
+                        ))
+                    }
+                </div>
 
-            <div>
-                {
-                    isFavorite ? (
-                        <FavoriteIcon style={{
-                            fontSize: 40,
-                        }} />
-                    ) : (
-                        <FavoriteBorderIcon  style={{
-                            fontSize: 40,
-                        }}/>
-                    )
-                }
+                <div style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    gap: 10,
+                }}>
+                    <div>
+                        {
+                            isFavorite ? (
+                                <FavoriteIcon style={{
+                                    fontSize: 40,
+                                }} />
+                            ) : (
+                                <FavoriteBorderIcon style={{
+                                    fontSize: 40,
+                                }} />
+                            )
+                        }
+                    </div>
+                
+                    <div>
+                       <TuneIcon />
+                    </div>
+                </div>
             </div>
         </div>
     )
